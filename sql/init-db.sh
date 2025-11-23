d9ea6009458151a8176537ede625c254f78176ad
akilah <akilah.medici@hotmail.com>
2025-11-23 00:21:44 -0300

    Revert "feat: added automated insert db on docker compose up"
    This reverts commit 3b3aee456e0cd26e6104dc588a180fda0fe5fbc2.

--- sql/init-db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "Password123" -C -i /sql/create-database.sql
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "Password123" -C -i /sql/initial-population-data.sql
-# Add more scripts as needed
-exec /opt/mssql/bin/sqlservr
